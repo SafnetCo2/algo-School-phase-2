4f08640ae97529d5e87000881c6663c9a925fd13
SafnetCo2 <nzi19001@byui.edu>
2023-10-18 18:13:59 +0300

    multipklication score not yet working

---
+++ score-right/script.js
@@ -0,0 +1,38 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+const questionEl = document.getElementById('question');
+const scoreEl = document.getElementById('score');
+
+
+
+const formEl = document.getElementById('form')
+
+
+
+let score = JSON.parse(localStorage.getItem('score'));
+if (!score) {
+    score = 0;
+}
+scoreEl.innerText = `score: ${score}`;
+questionEl.innerText = `what is ${num1} multiplied ${num2}?`;
+const inputEl= document.getElementById('form')
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener('submit', () => {
+    const userAns = +inputEl.value;
+    // console.log(userAns, typeof userAns);
+    if (userAns === correctAns) {
+        score++;
+        updateStorage();
+
+        
+    } else {
+        score--;
+        updateStorage();
+    }
+});
+function updateStorage() {
+    updateStorage.setItem('score', JSON.stringify(score));
+}
+
