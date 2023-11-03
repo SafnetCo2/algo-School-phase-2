57c877710ba401bfc1b52833600e3da63842232d
SafnetCo2 <nzi19001@byui.edu>
2023-11-03 12:11:49 +0300

    learning javascript

---
+++ SCRIPT-LEARNING/script.js
@@ -0,0 +1,29 @@
+let greet;
+greet = 'Hello World';
+greet = 'Enyy';
+console.log(greet);
+
+//output enny why When the value is changed, the old data is removed from the variable
+
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+
+console.log($ + _); // 3
+
+let admin, name; //can declare two variables once
+
+name = 'john';
+admin = name;
+console.log(admin);
+
+
+// let n = 'kelly';
+// n = 1234;
+// console.log(n);
+// typeof (n);
+
+//logic
+let hour = 9;
+if (hour < 9 || hour < 10) {
+    alert('office clost')
+}
