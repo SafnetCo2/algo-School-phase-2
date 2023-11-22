1ac8e75bb0d8ca52bfac48904a3dafd08dba3181
SafnetCo2 <nzi19001@byui.edu>
2023-11-22 13:27:30 +0300

    variables,create,store

---
+++ js-practice-freecodecamp/storeVariable.js
@@ -0,0 +1,10 @@
+
+/**First, this code creates a variable named myVar.Then, the code assigns 5 to myVar.Now, if myVar appears again in the code, the program will treat it as if it is 5. */
+
+var myName;
+myName = 'Josephine';//myname variable stores my name using assignement operator equal
+console.log(myName);
+
+var a;
+a = 5;
+console.log(a);
---
+++ js-practice-freecodecamp/varibles.js
@@ -0,0 +1,8 @@
+var myName;//create a variable
+/**Variables allow computers to store and manipulate data in a dynamic fashion. They do this by using a "label" to point to the data rather than using the data itself. Any of the eight data types may be stored in a variable.
+
+Variables are similar to the x and y variables you use in mathematics, which means they're a simple name to represent the data we want to refer to. Computer variables differ from mathematical variables in that they can store different values at different times.
+
+We tell JavaScript to create or declare a variable by putting the keyword var in front of it, like so:
+
+var ourName; */
